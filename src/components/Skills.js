fbeb4a70181930b00e2dd2fda2756512757fd64d
Wendell Powery <126547506+110220004@users.noreply.github.com>
2024-06-19 09:52:38 -0600

    added code to skills.js
    pending for more information

---
+++ src/components/Skills.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <div>
+      <h2>Skills</h2>
+      <ul>
+        <li>nothing</li>
+        <li>nothing</li>
+        <li>nothing</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Skills;
