53d026a81d3fe06ecd2f1461790bb936d51141c2
Wendell Powery <126547506+110220004@users.noreply.github.com>
2024-06-19 11:04:50 -0600

    updated skill.js
    made some adjustments to the code

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Skills = () => {
+  const [skills] = useState([
+    {
+      name: 'python',
+      icon: 'fas fa-html5'
+    },
+    {
+      name: 'Java',
+      icon: 'fas fa-css3-alt'
+    },
+    {
+      name: 'Android Studio',
+      icon: 'fas fa-js'
+    },
+  ]);
+
   return (
-    <div>
-      <h2>Skills</h2>
-      <ul>
-        <li>nothing</li>
-        <li>nothing</li>
-        <li>nothing</li>
+    <div className="skills-container">
+      <h2 className="title">Skills</h2>
+      <ul className="skill-list">
+        {skills.map((skill, index) => (
+          <li key={index}>
+            <i className={skill.icon} />
+            <h3>{skill.name}</h3>
+          </li>
+        ))}
       </ul>
     </div>
   );
