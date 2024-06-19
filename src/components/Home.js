a0bea4942fb09e180865a8dfb2d3452f6c7c6264
Wendell Powery <126547506+110220004@users.noreply.github.com>
2024-06-19 09:50:09 -0600

    added code to home.js
    basically my name and what i do

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Wendell Powery</h1>
+      <p>Frontend Developer</p>
+    </div>
+  );
+};
+
+export default Home;
