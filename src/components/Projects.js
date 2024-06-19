a93b68a84078fc54faa4fd5e88ba0eab2b5c591a
Wendell Powery <126547506+110220004@users.noreply.github.com>
2024-06-19 09:55:10 -0600

    added code to projects.js
    added the funny

---
+++ src/components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div>
+      <h2>Projects</h2>
+      <ul>
+        <li>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">If your interested in something funny</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Projects;
