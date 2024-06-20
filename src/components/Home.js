c2de5a5dc73ebefd891abb1ba589c669c3b24efd
Wendell Powery <126547506+110220004@users.noreply.github.com>
2024-06-19 21:05:14 -0600

    made some adjustments to home.js
    got rid of WhatsApp option and added my own links to my socials

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,19 +2,15 @@ import React, { useState } from 'react';
 
 const Socials = () => (
   <div className="socials-container">
-    <a href="https://www.facebook.com/yourusername" target="_blank" rel="noopener noreferrer">
+    <a href="https://www.facebook.com/levy.powery/" target="_blank" rel="noopener noreferrer">
       Facebook
       <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook" />
     </a>
-    <a href="https://www.instagram.com/yourusername" target="_blank" rel="noopener noreferrer">
+    <a href="https://www.instagram.com/powerylevy/" target="_blank" rel="noopener noreferrer">
       Instagram
       <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram" />
     </a>
-    <a href="https://wa.me/yourphonenumber" target="_blank" rel="noopener noreferrer">
-      WhatsApp
-      <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" />
-    </a>
-    <a href="mailto:youremail@example.com">
+    <a href="mailto:levypowery@gmail.com">
       Email
       <img src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg" alt="Email" />
     </a>
